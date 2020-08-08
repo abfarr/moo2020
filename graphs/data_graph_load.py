fc3d9861ceda2e690998127b78c7ecb03c537c54
Alexi Farr <61439303+abfarr@users.noreply.github.com>
2020-08-08 15:48:42 -0400

    Update data_graph_load.py

--- graphs/data_graph_load.py
+++ graphs/data_graph_load.py
@@ -1,8 +1,5 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-<<<<<<< HEAD
-from random import sample
-=======
 import random
 
 random.seed(42)
@@ -13,16 +10,9 @@ def knbrs(G, start, k):
         newnbrs = set((nbr for n in nbrs for nbr in G[n]))
         nbrs=newnbrs |nbrs
     return nbrs
->>>>>>> 7a164bf6cd2f790801f77a40fb6ac9241c97e21f
 
 G = nx.read_adjlist('../data/inf-power/inf-power.mtx')
-random_subnodes = sample(G.nodes,20)
-g = G.subgraph(random_subnodes)
 
-<<<<<<< HEAD
-nx.draw_spring(g)
-plt.show()
-=======
 # subgraph_main_node = random.sample(G.nodes, 5)
 
 mapping = dict(zip(G, range(0, len(G))))
@@ -33,4 +23,3 @@ print(G.nodes)
 # nx.draw_spring(knbrs(G, main_node=4, k=2), node_size=200, width=1.5, with_labels=True)
 # plt.show()
 # knbrs(G, main_node, k=1)
->>>>>>> 7a164bf6cd2f790801f77a40fb6ac9241c97e21f

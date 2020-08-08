273150bf0ab7bb2103e84e811c7c9e63ccc83360
David Farr <davidthfarr@gmail.com>
2020-08-08 15:45:39 -0400

    initial work

--- graphs/data_graph_load.py
+++ graphs/data_graph_load.py
@@ -1,7 +1,10 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+from random import sample
 
 G = nx.read_adjlist('../data/inf-power/inf-power.mtx')
+random_subnodes = sample(G.nodes,20)
+g = G.subgraph(random_subnodes)
 
-nx.draw(G)
+nx.draw_spring(g)
 plt.show()

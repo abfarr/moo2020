bdff542f02bfb324138ab3e3df5d9e5cf1a9e60f
abfarr <alexibell13@gmail.com>
2020-09-15 21:10:25 -0400

    added pulp

--- graphs/data_graph_functions.py
+++ graphs/data_graph_functions.py
@@ -26,31 +26,10 @@ def load_graph(file_loc='../data/inf-power/inf-power.mtx'):
     return G
 
 
-def knbrs(G, start, k):
-    """Gets all nodes in graph G at or within k hops of start node
-
-    :type G: networkx.Graph
-    :param G: Networkx graph of interest
-    :type start: int, string
-    :param start:  Name of initial node to use to build the extended neighborhood from
-    :type k: int
-    :param k: Number of hops from start node to go out to build extended neighborhood
-
-    :rtype nbrs: set
-    :return nbrs: Neighbors of start node at or within k hops away
-    """
-
-    nbrs = {start}
-    for l in range(k):
-        new_nbrs = set((nbr for n in nbrs for nbr in G[n]))
-        nbrs = new_nbrs | nbrs
-    return nbrs
-
-
 def xsubgraphs(G, x, k, seed=42):
-    """Randomly selects x number of subgraphs from G, where k is the number of steps from central nodes to use
-    to create a connected subgraph
-    
+    """Randomly selects x number of subgraphs from G, where k is the number of steps from the central node
+    of each subgraphto use
+
     :type G: networkx.Graph
     :param G: Networkx graph of interest
     :type x: int
@@ -69,13 +48,142 @@ def xsubgraphs(G, x, k, seed=42):
 
     subgraphs = []
     for n in start_nodes:
-        subgraph_nodes = knbrs(G, start=n, k=k)
-        subgraphs.append(G.subgraph(subgraph_nodes))
+        g = nx.ego_graph(G, n, k)
+        d = {list(g.nodes)[i]: 0 for i in range(len(list(g.nodes)))}
+        d[n] = 1
+        nx.set_node_attributes(g, d, 'start_nodes')
+        subgraphs.append(g)
+
+    return subgraphs
+
+
+def plot_T0(G):
+    colors = []
+    for n in G.nodes:
+        if G.nodes[n]['start_nodes'] == 1:
+            colors.append('r')
+        else:
+            colors.append('b')
+
+    # drawing nodes and edges separately so we can capture collection for colobar
+    pos = nx.spring_layout(G)
+    ec = nx.draw_networkx_edges(G, pos, alpha=0.2)
+    nc = nx.draw_networkx_nodes(G, pos, nodelist=G.nodes, node_color=colors,
+                                with_labels=True, node_size=300)
+    lb = nx.draw_networkx_labels(G, pos, font_size=10)
+    # plt.colorbar(nc)
+    plt.axis('off')
+    plt.show()
+
+
+def get_fig(graph, time, graph_type_proper, algorithm_proper, f, k, title_ONOFF):
+    neutral = []
+    safe = []
+    burned = []
+    for n in graph:
+        if nx.get_node_attributes(graph, 'b/n/d')[n] == 0:
+            neutral.append(n)
+        elif nx.get_node_attributes(graph, 'b/n/d')[n] == 1:
+            safe.append(n)
+        else:
+            burned.append(n)
+
+    nodes = {}
+    for n in list(graph.nodes):
+        nodes[n] = (n)
+
+    # pos = nx.get_node_attributes(graph, 'pos')
+    pos = nx.spring_layout(graph)
+    nx.set_node_attributes(graph, pos, 'pos')
+
+    fig = plt.figure(figsize=(6,7.2))#pylab.figure(figsize=(6,6))
+    ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+    nx.draw_networkx_nodes(graph, nx.get_node_attributes(graph, 'pos'), nodelist=neutral, node_size=200,
+                           node_color='b', label='unburnt/\nundefended')
+    nx.draw_networkx_nodes(graph, nx.get_node_attributes(graph, 'pos'), nodelist=safe, node_size=200,
+                           node_color='g', label='defended')
+    nx.draw_networkx_nodes(graph, nx.get_node_attributes(graph, 'pos'), nodelist=burned, node_size=200,
+                           node_color='r', label='burnt')
+    nx.draw_networkx_edges(graph, nx.get_node_attributes(graph, 'pos'), width=2, edge_color='k')
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0+0.1, box.width, box.height*0.8])
+    #nx.draw_networkx_labels(graph, pos, font_size=10, font_color='w')
+    plt.legend(bbox_to_anchor=(0.5, -0.3),loc='lower center', fontsize=12)
+    if title_ONOFF == 'ON':
+        if f == 1 and k == 1:
+            plt.title('FP with ${}$ for MAXSAVE and $t$-FIRE\non {}\nat Time $t$={} for {} Fire and {} Firefighter'.format(
+                algorithm_proper,graph_type_proper,time, f, k), fontsize=15)
+        elif f == 1 and k > 1:
+            plt.title('FP with ${}$ for $t$-FIRE on {} \nat Time $t$={} for {} Fire and {} Firefighters'.format(
+                algorithm_proper,graph_type_proper, time, f,k), fontsize=15)
+        elif f > 1 and k==1:
+            plt.title('FP with ${}$ for $t$-FIRE on {} \nat Time $t$={} for {} Fires and {} Firefighter'.format(
+                algorithm_proper,graph_type_proper,time, f,k), fontsize=15)
+        else:
+            plt.title('FP with ${}$ for $t$-FIRE on {} \nat Time $t$={} for {} Fires and {} Firefighters'.format(
+                algorithm_proper, graph_type_proper, time, f, k), fontsize=15)
+
+    return fig
+
+
+## DON'T USE UNLESS YOU FIX IT
+def xsubgraphs2(G, x, p, k, seed=42):
+    """Randomly selects x number of subgraphs from G, where k is the number of steps from the central node
+    of each subgraphto use
+    !!!! WILL NOT NECESSARILY CREATE CONNECTED GRAPH, DO NOT USE UNLESS YOU FIX IT
+
+    :type G: networkx.Graph
+    :param G: Networkx graph of interest
+    :type x: int
+    :param x: Number of subgraphs to build
+    :type p: int
+    :param p: number of start_nodes to use
+    :type k: int
+    :param k: Number of hops from start node to go out to build extended neighborhood
+    :type seed: int
+    :param seed: Number used to set the random seed to make results reproducible
+
+    :rtype subgraphs: list
+    :return subgraphs: List containing x number of randomly selected subgraphs from G
+    """
+
+    random.seed(seed)
+
+    subgraphs = []
+
+    for sg in range(x):
+        # Select p number of start nodes at random
+        start_nodes = random.sample(G.nodes, p)
+
+        subs = []
+        for n in start_nodes:
+            # Get ego graph (k steps out) of each start node
+            g = nx.ego_graph(G, n, k)
+            # Append nodes of g to list of nodes in subgraph
+            subs.append(list(g.nodes))
+
+        # Flatten list, keep only unique, and get subgraph including all nodes
+        sub = list(set([item for sublist in subs for item in sublist]))
+        g = nx.subgraph(G, sub)
+
+        # Create dictionary for defining start_node attributes (0 if not in start nodes)
+        d = {list(g.nodes)[i]: 0 for i in range(len(list(g.nodes)))}
+        for n in start_nodes:
+            d[n] = 1 #1 if in start nodes
+
+        # Set start_node attributes
+        nx.set_node_attributes(g, d, 'start_nodes')
+
+        # Append graph to list of subgraphs
+        subgraphs.append(g)
 
     return subgraphs
 
 # # Example
 # G = load_graph(file_loc='../data/inf-power/inf-power.mtx')
-# subgraphs = xsubgraphs(G, x=5, k=5, seed=42)
-# nx.draw_spring(subgraphs[0], node_size=200, width=1.5, with_labels=True)
-# plt.show()
+# subgraphs = xsubgraphs(G, x=5, k=2, seed=42)
+# print(nx.get_node_attributes(subgraphs[0], 'start_nodes'))
+# for g in subgraphs:
+#     plot_T0(g)
+
+
--- weighted_moo/simple_moo.py
+++ weighted_moo/simple_moo.py
@@ -1,22 +1,17 @@
 from pulp import *
-from graphs import data_graph_functions
+from graphs import data_graph_functions as dgf
 import networkx as nx
 import matplotlib.pyplot as plt
+import operator
+import numpy as np
+import pandas as pd
 
-G = nx.ladder_graph(10)
-# nx.draw_spring(G, node_size=200, width=1.5, with_labels=True)
-# plt.show()
+def solve_lp(G1, T_init, num_ffs, start_nodes, alpha):
 
-def solve_lp(G1, T_init, num_fires, num_ffs, savable_proportion, start_nodes):
-    #Set constants
-    V = G1.number_of_nodes()
-
-    # Determine number of firefighters, initial fires, and start fire(s)
+    # Determine number of firefighters
     p = num_ffs
-    f = num_fires
 
-    # Set savable proportion (0 if only saving 1 node) and initial T
-    s = savable_proportion
+    # Initial T
     T = T_init
 
     # Refresh graph to original state
@@ -48,36 +43,29 @@ def solve_lp(G1, T_init, num_fires, num_ffs, savable_proportion, start_nodes):
                               lowBound=0, upBound=1, cat='Binary') # Inclusive bound, defended vars by node and time
 
     # Objective
-    prob += pulp.lpSum(b[(x, T)] - b[(x, T - 1)] for x in nodes), 'Obj for time {}'.format(T)
+    prob += lpSum(b[(x, T)]*alpha + (b[(x, T)]-b[(x, T-1)])*(1-alpha)), 'Obj for T={}, alpha={}'.format(T, alpha) # MAXSAVE * alpha + TFIRE * (1-alpha)
+    # prob += pulp.lpSum(b[(x, T)] - b[(x, T - 1)] for x in nodes), 'Obj for time {}'.format(T)
 
     # Constraints (spontaneous combustion if infeasible)
     for t in time1:
         for x in nodes:
             nbrs = neighbor_dict[x]
             for y in nbrs:
-                prob += - b[(x, t)] - d[(x, t)] + b[(y, t - 1)] <= 0  #
+                prob += - b[(x, t)] - d[(x, t)] + b[(y, t - 1)] <= 0  # neighbors of burning node must burn or be defended in next time step
 
-            prob += -pulp.lpSum(b[(y, t - 1)] for y in nbrs) + b[(x, t)] - b[(x, t - 1)] <= 0
-            prob += b[(x, t)] + d[(x, t)] <= 1
-            prob += b[(x, t - 1)] - b[(x, t)] <= 0
-            prob += d[(x, t - 1)] - d[(x, t)] <= 0
+            prob += -lpSum(b[(y, t - 1)] for y in nbrs) + b[(x, t)] - b[(x, t - 1)] <= 0 # prevent spontaneous combustion
+            prob += b[(x, t)] + d[(x, t)] <= 1 # can't be burnt and defended
+            prob += b[(x, t - 1)] - b[(x, t)] <= 0 # burnt stays burnt
+            prob += d[(x, t - 1)] - d[(x, t)] <= 0 # defended stays defended
 
-        if t < T:
-            prob += pulp.lpSum(d[(x, t)] - d[(x, t - 1)] for x in nodes) == p  # save exactly p ==
-        else:
-            prob += pulp.lpSum(d[(x, T)] - d[(x, T - 1)] for x in nodes) <= p  # except in final time step
-
-    if s > 0:  # Savable proportion case
-        prob += -(1 - (1 / V) * pulp.lpSum((b[(x, T)] + d[(x, T)]) for x in nodes)) <= -s  # save at least s proportion
-    else:  # Savable amount (1) case
-        prob += -(V - pulp.lpSum(b[(x, T)] + d[(x, T)] for x in nodes)) <= -1  # save at least 1
+        prob += pulp.lpSum(d[(x, T)] - d[(x, T - 1)] for x in nodes) <= p # ADD: use no more than the allotted number of firefighters
 
     for x in nodes:
         if x in start_nodes:
-            prob += (b[(x, 0)]) == 1
+            prob += (b[(x, 0)]) == 1 # init burn
         else:
-            prob += (b[(x, 0)]) == 0
-        prob += (d[(x, 0)]) == 0
+            prob += (b[(x, 0)]) == 0 # init unburnt
+        prob += (d[(x, 0)]) == 0 # init defended
 
     for x in nodes:
         if x not in start_nbrs_all and x not in start_nodes:
@@ -93,9 +81,174 @@ def solve_lp(G1, T_init, num_fires, num_ffs, savable_proportion, start_nodes):
 
     return prob, status, obj, vars
 
-print(solve_lp(G, 2, 1, 1, 0, [1,2]))
-#
-#
-# prob = LpProblem("MAXSAVE_tFIRE_Problem",LpMinimize)
-#
-# LpVariable("example", 0, 1, LpInteger)
+
+def find_optimal_T(G1, f, p, T, T_upper, alpha, plot):
+    G = G1.copy()
+    size = nx.number_of_nodes(G)
+
+    start_nodes = []
+    for n in G.nodes:
+        if G.nodes[n]['start_nodes'] == 1:
+            start_nodes.append(n)
+
+    optimal_T = False
+    while optimal_T != True and T <= T_upper:
+
+        prob, status, obj, vars = solve_lp(G, T_init=T, num_ffs=p, start_nodes=start_nodes, alpha=alpha)
+        #print('Status: {} \tObjective: {}\tStart Nodes: {}'.format(status,obj, start_nodes))
+
+
+        # Get all burnt throughout entire process. Use this to verify containment since obj == 0 does not apply with MAXSAVE
+        burnt = []
+        for v in vars:
+            if v.name.startswith('B') and v.varValue > 0:
+                node_b = int(v.name[v.name.find("(") + 1:v.name.find(",")])
+                time_b = int(v.name[v.name.find(",") + 2:v.name.find(")")])
+                burnt.append((node_b, time_b))
+
+        last_burnt = []
+        second_last_burnt = []
+        max_time = max(burnt, key=lambda x: x[1])[1]
+        for tup in burnt:
+            if tup[1] == max_time:
+                last_burnt.append(tup)
+            elif tup[1] == max_time - 1:
+                second_last_burnt.append(tup)
+        if len(list(set(last_burnt))) - len(list(set(second_last_burnt))) == 0:
+            contained=True
+        else:
+            contained=False
+
+        if status == 'Optimal' and contained == True:
+            optimal_T = True
+        elif status == 'Optimal' and contained == False:
+            optimal_T = False
+            T = T + 1
+        elif status == 'Infeasible':
+            optimal_T = False
+
+        # if optimal_T == True:
+        #     print('Time: {}\t\tOptimal: {}'.format(T, optimal_T))
+        # else:
+        #     print('Time: {}\t\tOptimal: {}'.format(T-1, optimal_T))
+
+    if optimal_T == True:
+        print('Optimal T Found: T = {}'.format(T-1))
+
+        # Get all defended nodes throughout entire process.
+        defended = []
+        for v in vars:
+            if v.name.startswith('D') and v.varValue > 0:
+                node_d = int(v.name[v.name.find("(") + 1:v.name.find(",")])
+                time_d = int(v.name[v.name.find(",") + 2:v.name.find(")")])
+                defended.append((node_d, time_d))
+
+        # Find earliest time step at which a node burns.
+        b_first = {}
+        for n in set([b[0] for b in burnt]):
+            b_times = []
+            for tup in burnt:
+                if tup[0] == n:
+                    b_times.append(tup[1])
+            for tup in burnt:
+                if tup[0] == n and tup[1] == min(b_times):
+                    b_first[tup[0]] = min(b_times)
+        b_first = dict(sorted(b_first.items(), key=operator.itemgetter(1)))
+
+        # Find earliest time step at which a node is defended.
+        d_first = {}
+        for n in set([d[0] for d in defended]):
+            d_times = []
+            for tup in defended:
+                if tup[0] == n:
+                    d_times.append(tup[1])
+            for tup in defended:
+                if tup[0] == n and tup[1] == min(d_times):
+                    d_first[tup[0]] = min(d_times)
+        d_first = dict(sorted(d_first.items(), key=operator.itemgetter(1)))
+
+        containment = True
+        tot_burnt = len(b_first)
+        tot_defended = len(d_first)
+        tot_saved = size - tot_burnt
+        tot_saved_indirect = size - tot_burnt - tot_defended
+
+        if plot == True:
+            for n in list(G.nodes):
+                G.nodes[n]['b/n/d'] = 0
+            for n in b_first.keys():
+                G.nodes[n]['b/n/d'] = -1
+            for n in d_first.keys():
+                G.nodes[n]['b/n/d'] = 1
+
+            dgf.get_fig(graph=G,
+                        time=T,
+                        graph_type_proper = 'Subgraph of Grid',
+                        algorithm_proper = 'Simple Weighted MOO LP',
+                        f=f,
+                        k=p,
+                        title_ONOFF='ON')
+            #nx.draw(G1, pos=nx.get_node_attributes(G1, 'pos'))
+            nx.draw_networkx_labels(G, pos=nx.get_node_attributes(G, 'pos'), font_color='w')
+            plt.show()
+    else:
+        containment = False
+        tot_burnt = 0
+        tot_defended = 0
+        tot_saved = 0
+        tot_saved_indirect = 0
+        print('NO OPTIMAL SOLUTION FOR T_MAX = {}'.format(T-1))
+
+    return T-1, tot_burnt, tot_defended, tot_saved, tot_saved_indirect, containment
+
+
+# G = nx.ladder_graph(10)
+# nx.draw_spring(G, node_size=200, width=1.5, with_labels=True)
+# plt.show()
+
+# print(solve_lp(G,
+#                T_init=2,
+#                num_ffs=1,
+#                start_nodes=[1,2],
+#                alpha=0.5))
+
+T_max = 5
+G = dgf.load_graph(file_loc='../data/inf-power/inf-power.mtx')
+subgraphs = dgf.xsubgraphs(G, x=5, k=T_max+2, seed=42)
+
+g = subgraphs[0]
+# start_nodes = []
+# for n in g.nodes:
+#     if g.nodes[n]['start_nodes'] == 1:
+#         start_nodes.append(n)
+# print(solve_lp(g,
+#                T_init=1,
+#                num_ffs=1,
+#                start_nodes=start_nodes,
+#                alpha=0.5))
+
+alpha_vals = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+results = []
+G_num = 0
+for g in subgraphs:
+    for a in alpha_vals:
+        T, tot_burnt, tot_defended, tot_saved, tot_saved_indirect, containment = \
+            find_optimal_T(g,
+                       f=1,
+                       p=1,
+                       T=1,
+                       T_upper=T_max,
+                       alpha=a,
+                       plot=False)
+        ans_val = [G_num, a, T, tot_burnt, tot_defended, tot_saved, tot_saved_indirect, containment]
+        ans_key = ['G Num', 'Alpha', 'T', 'Burnt', 'Defended',
+                   'Saved', 'Saved (Indirect)',
+                   'Contained']
+        ans_dict = {ans_key[i]:ans_val[i] for i in range(len(ans_key))}
+        results.append(ans_dict)
+    G_num+=1
+
+df = pd.DataFrame(results)
+print(df)
+
+# dgf.plot_T0(g)
